aed5d3c19d8571d470bf2bbfb7d0595bfb90d73f
Vladimir Iftodi <vladimir.iftodi@gmail.com>
2015-07-19 19:12:45 +0200

    Allowing to specify a tag when adding an entry

--- wMRU.ml
+++ wMRU.ml
@@ -3,24 +3,27 @@ open Batteries
 let fname = Sys.getenv "HOME" ^ "/.wMRU"
 let rw_perm = BatFile.perm [BatFile.user_read ; BatFile.user_write]
 
+let get_path fname = Sys.getenv "HOME" ^ "/." ^ fname
+
 let print_line line = print_string (line  ^ "\n" )
 
-let read_lines () = BatFile.with_file_in
+let read_lines fname = BatFile.with_file_in
                      ~mode:[`create]
 		     ~perm:rw_perm
-		     fname
+		     (get_path fname)
 		     (fun fh -> BatList.of_enum (BatIO.lines_of fh))
 
-let do_get () = List.iter print_line  (read_lines () )
+let do_get fname = List.iter print_line  (read_lines fname )
 
-let do_add () = let args = List.tl (List.tl (Array.to_list Sys.argv)) in
+let do_add fname = let args = List.tl (List.tl (List.tl (Array.to_list Sys.argv))) in
 			    let line = String.concat " " args in
-			    let lines = List.filter (fun s -> not (BatString.equal s line)) (read_lines ()) in
-			    BatFile.write_lines fname (BatList.enum (line::lines))
+			    let lines = List.filter (fun s -> not (BatString.equal s line)) (read_lines fname) in
+			    BatFile.write_lines (get_path fname) (BatList.enum (line::lines))
 
-let do_command () = match Sys.argv.(1) with
- | "get" -> do_get ()
- | "add" -> do_add()
- | _ 	 -> print_string "You must not be walt"
+let do_command () = let fname = Sys.argv.(2) in
+		match Sys.argv.(1) with
+		| "get" -> do_get fname
+		| "add" -> do_add fname
+		| _ 	 -> print_string "You must not be walt"
 					
 let () = do_command ()
